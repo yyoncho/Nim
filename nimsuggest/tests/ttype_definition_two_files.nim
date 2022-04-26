9ea4e74a05b0bc0265f378da955e7186ea74b560
Ivan Yonchovski <yyoncho@gmail.com>
2022-04-26 17:23:50 +0300

    Reproducer

---
+++ nimsuggest/tests/ttype_definition_two_files.nim
@@ -0,0 +1,11 @@
+# Test def with type definition in separate file
+
+discard """
+$nimsuggest --tester /home/yyoncho/Sources/nim/Nim/nimsuggest/tests/fixtures/nimble.nim
+>def $path/fixtures/nimble.nim:4:16
+def	skField	packageinfotypes.PackageInfo.isMinimal	string	*nimsuggest/tests/fixtures/packageinfotypes.nim	3	4	""	100
+>def $path/fixtures/packageinfotypes.nim:3:4
+def	skField	packageinfotypes.PackageInfo.sMinimal		*nimsuggest/tests/fixtures/packageinfotypes.nim	3	4	""	100
+>def $path/fixtures/nimble.nim:4:15
+def	skField	packageinfotypes.PackageInfo.isMinimal	string	*nimsuggest/tests/fixtures/packageinfotypes.nim	3	4	""	100
+"""
